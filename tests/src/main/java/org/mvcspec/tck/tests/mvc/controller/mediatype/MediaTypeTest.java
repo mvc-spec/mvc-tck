ab2fae0d1a1578dd126e4c6b85b4c4d53064ae81
Christian Kaltepoth <christian@kaltepoth.de>
2018-04-04 08:45:55 +0200

    Added tests for default media type

---
+++ tests/src/main/java/org/mvcspec/tck/tests/mvc/controller/mediatype/MediaTypeTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2017 Ivar Grimstad (ivar.grimstad@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mvcspec.tck.tests.mvc.controller.mediatype;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mvcspec.tck.Sections;
+import org.mvcspec.tck.util.Archives;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Arquillian.class)
+@SpecVersion(spec = "mvc", version = "1.0")
+public class MediaTypeTest {
+
+    @ArquillianResource
+    private URL baseUrl;
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return Archives.getMvcArchive()
+                .addClass(MediaTypeController.class)
+                .addView("Some rendered view", "view.jsp")
+                .build();
+    }
+
+    /**
+     * Moreover, the default media type for a response is assumed to be text/html, but otherwise can be declared
+     * using @Produces just like in JAX-RS
+     */
+    @Test
+    @SpecAssertion(section = Sections.MVC_CONTROLLERS, id = "default-mediatype")
+    public void defaultMediaType() throws IOException {
+
+        WebClient client = new WebClient();
+
+        // default content-type of text/html
+        WebResponse response1 = client.getPage(baseUrl.toString() + "mvc/mediatype/default").getWebResponse();
+        assertThat(response1.getStatusCode(), equalTo(200));
+        assertThat(response1.getContentType(), equalTo("text/html"));
+        assertThat(response1.getContentAsString(), containsString("Some rendered view"));
+
+        // custom content-type of text/plain
+        WebResponse response2 = client.getPage(baseUrl.toString() + "mvc/mediatype/custom").getWebResponse();
+        assertThat(response2.getStatusCode(), equalTo(200));
+        assertThat(response2.getContentType(), equalTo("text/plain"));
+        assertThat(response2.getContentAsString(), containsString("Some rendered view"));
+
+    }
+
+}

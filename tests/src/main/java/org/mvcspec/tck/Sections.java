d96143cd640af4f15fbcc3782cc85965f3078600
Christian Kaltepoth <christian@kaltepoth.de>
2018-04-04 08:38:51 +0200

    Added manually created section class

---
+++ tests/src/main/java/org/mvcspec/tck/Sections.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2017 Ivar Grimstad (ivar.grimstad@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mvcspec.tck;
+
+public class Sections {
+
+    /*
+     * Chapter: Models, Views and Controllers
+     */
+    public static final String MVC_CONTROLLERS = "controllers";
+    public static final String MVC_CONTROLLER_INSTANCES = "controller_instances";
+    public static final String MVC_RESPONSE = "response";
+    public static final String MVC_REDIRECT = "redirect";
+    public static final String MVC_MODELS = "models";
+    public static final String MVC_VIEWS = "views";
+    public static final String MVC_BUILDING_URIS = "mvc_uri";
+
+}

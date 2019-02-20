3405dfa93df26b02c8df0e3127a3a3db2f09735d
Christian Kaltepoth <christian@kaltepoth.de>
2019-02-20 17:40:41 +0100

    Added test for i18n data binding for floats

---
+++ tests/src/main/java/org/mvcspec/tck/tests/binding/numeric/BindingFloatForm.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2017 Ivar Grimstad (ivar.grimstad@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mvcspec.tck.tests.binding.numeric;
+
+import javax.mvc.binding.MvcBinding;
+import javax.ws.rs.FormParam;
+
+public class BindingFloatForm {
+
+    @MvcBinding
+    @FormParam("value")
+    private float primitive;
+
+    @MvcBinding
+    @FormParam("value")
+    private Float object;
+
+    public float getPrimitive() {
+        return primitive;
+    }
+
+    public void setPrimitive(float primitive) {
+        this.primitive = primitive;
+    }
+
+    public Float getObject() {
+        return object;
+    }
+
+    public void setObject(Float object) {
+        this.object = object;
+    }
+    
+}

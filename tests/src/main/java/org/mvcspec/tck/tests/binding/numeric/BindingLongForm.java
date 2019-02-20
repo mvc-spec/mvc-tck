7e2da023b990e462d46b42e57cd8fc8caf5041cd
Christian Kaltepoth <christian@kaltepoth.de>
2019-02-20 17:47:04 +0100

    Added test for i18n data binding for longs

---
+++ tests/src/main/java/org/mvcspec/tck/tests/binding/numeric/BindingLongForm.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2017 Ivar Grimstad (ivar.grimstad@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mvcspec.tck.tests.binding.numeric;
+
+import javax.mvc.binding.MvcBinding;
+import javax.ws.rs.FormParam;
+
+public class BindingLongForm {
+
+    @MvcBinding
+    @FormParam("value")
+    private long primitive;
+
+    @MvcBinding
+    @FormParam("value")
+    private Long object;
+
+    public long getPrimitive() {
+        return primitive;
+    }
+
+    public void setPrimitive(long primitive) {
+        this.primitive = primitive;
+    }
+
+    public Long getObject() {
+        return object;
+    }
+
+    public void setObject(Long object) {
+        this.object = object;
+    }
+}
